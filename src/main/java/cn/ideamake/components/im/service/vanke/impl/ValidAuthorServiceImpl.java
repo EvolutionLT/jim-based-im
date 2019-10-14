32f97829eacfd1e7e8aec472cf99c21a3b19dc91
apollo <duping@iteamake.cn>
2019-10-14 18:23:08 +0800

    "vanke-业务"

--- src/main/java/cn/ideamake/components/im/service/vanke/impl/ValidAuthorServiceImpl.java
+++ src/main/java/cn/ideamake/components/im/service/vanke/impl/ValidAuthorServiceImpl.java
@@ -222,18 +222,6 @@ public class ValidAuthorServiceImpl implements ValidAuthorService {
         dto.setReceiverId(to);
 //        RedisCacheManager.getCache(ImConst.USER).incr(String.format(VankeRedisKey.VANKE_CHAT_MEMBER_NUM_KEY, to), 1);
         User friend = cacheFriend(dto);
-        // 模拟访客给客服发送消息
-        ImAio.sendToUser(user, friend.getId(), "您好, 我想咨询下楼盘信息！", "false");
-        // 模拟客服给访客发送消息
-        CompletableFuture.runAsync(() -> {
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                //TODO
-            }
-            ImAio.sendToUser(user, friend.getId(), "您好, 我想咨询下楼盘信息！", "false");
-//        ImAio.sendToUser(user, friend.getId(), "您好, 我想咨询下楼盘信息！", "false");
-        }, threadPoolExecutor);
 //        CompletableFuture.runAsync(() -> {
 //            try {
 //                Thread.sleep(500);
